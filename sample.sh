720bdf2100ca378cdd4065a3b38dd20843a80018
sureshk <sureshgk257@gmail.com>
2018-12-07 01:12:29 -0500

    Added script

---
+++ sample.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "This is the script to add numbers"
+echo "Firstnumber is $FIRSTNUMBER"
+echo "Second number is $SECONDNUMBER"
+NUM=$(($FIRSTNUMBER + $SECONDNUMBER))
+echo "Total is $NUM"
